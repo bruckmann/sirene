89ec86cc5bf2e3b4c2c852f01fad7dd32fb1a1e1
Felipe Bruckmann <felipebruckmann@hotmail.com.br>
2022-11-17 11:37:47 -0300

    Fixing meta tags for http request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Container from '../components/Container'
 import Image from '../components/Image'
 import Form from '../components/Form'
 
 const Home: NextPage = () => {
   return (
+    <>
+    <Head>
+      <title>Keratoconus Detection</title>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+    </Head>
     <div>
       <Container>
         <Image />
         <Form />
       </Container>
     </div>
+    </>
   )
 }
 
